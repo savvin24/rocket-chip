savvin24/rocket-chip

commit 988a659fffb7098fa1bcc26f984003f17b042288

Ameliorate BusErrorUnit critical path

src/main/scala/rocket/BusErrorUnit.scala
@@ -5,6 +5,7 @@ package freechips.rocketchip.rocket
 import Chisel._
 import Chisel.ImplicitConversions._
 import chisel3.util.Valid
+import chisel3.core.DontCare
 import freechips.rocketchip.config.Parameters
 import freechips.rocketchip.util._
 import freechips.rocketchip.tile._
@@ -84,15 +85,24 @@ class BusErrorUnit[T <: BusErrors](t: => T, params: BusErrorUnitParams)(implicit
       else RegFieldDesc.reserved
     }
 
+    val cause_wen = Wire(init = false.B)
+    val new_cause = Wire(UInt(causeWidth.W), DontCare)
+    val new_value = Wire(UInt(value.getWidth.W), DontCare)
     for ((((s, en), acc), i) <- (sources zip enable zip accrued).zipWithIndex; if s.nonEmpty) {
       when (s.get.valid) {
         acc := true
-        when (en && cause === 0) {
-          cause := i
-          value := s.get.bits
+        when (en) {
+          cause_wen := true
+          new_cause := i
+          new_value := s.get.bits
         }
         cover(en, s"BusErrorCause_$i", s"Core;;BusErrorCause $i covered")
       }
+    }
+
+    when (cause =/= 0 && cause_wen) {
+      cause := OptimizationBarrier(new_cause)
+      value := OptimizationBarrier(new_value)
     }
 
     val (int_out, _) = intNode.out(0)

commit 70e8b44c982f8866e2086e7eb599e5f7f74c7273

Fix BusErrorUnit regression (#1600)
I broke this in 988a659fffb7098fa1bcc26f984003f17b042288

I will investigate why regression testing didn't catch this.

src/main/scala/rocket/BusErrorUnit.scala
@@ -100,7 +100,7 @@ class BusErrorUnit[T <: BusErrors](t: => T, params: BusErrorUnitParams)(implicit
       }
     }
 
-    when (cause =/= 0 && cause_wen) {
+    when (cause === 0 && cause_wen) {
       cause := OptimizationBarrier(new_cause)
       value := OptimizationBarrier(new_value)
     }